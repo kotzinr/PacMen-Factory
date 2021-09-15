e781da51ffdd9924a35bf8a14184ae9efec33662
Roxy Kotzin <86859421+kotzinr@users.noreply.github.com>
2021-09-15 07:18:52 -0700

    Add files via upload

--- pacmen.js
+++ pacmen.js
@@ -1,7 +1,7 @@
 let pos = 0;
 const pacArray = [
-  ['./images/PacMan1.png', './images/PacMan2.png'],
-  ['./images/PacMan3.png', './images/PacMan4.png'],
+  ['PacMan1.png', 'PacMan2.png'],
+  ['PacMan3.png', 'PacMan4.png'],
 ];
 let direction = 0;
 const pacMen = []; // This array holds all the pacmen
@@ -24,7 +24,7 @@ function makePac() {
   let game = document.getElementById('game');
   let newimg = document.createElement('img');
   newimg.style.position = 'absolute';
-  newimg.src = './images/PacMan1.png';
+  newimg.src = 'PacMan1.png';
   newimg.width = 100;
 
   // TODO: set position here
